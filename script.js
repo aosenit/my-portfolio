fe3cac803e2c16255fe6facc4974e1596a84aec1
Adedoyin <creativitytraz22@gmail.com>
2021-01-25 14:55:23 +0100

    updated Portfolio

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const myLinks = document.getElementById('my-links')
+const myLinks = document.querySelector('.my-links')
 
 const toggleBar = document.getElementById('nav-toggle') 
 
-const myMenus = document.querySelectorAll('.nav-link ')
+const myMenus = document.querySelectorAll('.nav-link')
 
 
 toggleBar.addEventListener('click', () => {
 
     myLinks.classList.toggle('show')
-    toggleBar.classList.toggle('menubarChange')
+   
     
 } )
 
@@ -17,7 +17,57 @@ function HandleMenus() {
     this.classList.add('current-page')
 
     myLinks.classList.toggle('show')
-    toggleBar.classList.toggle('menubarChange')
+  
 }
 
 myMenus.forEach((myMenu) => { myMenu.addEventListener('click', HandleMenus )})
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+const ft = ScrollReveal({
+    origin: 'left',
+    distance: '800px',
+    duration: 2000,
+    reset: true
+});
+
+/*.............OTHER ANIMATIONS..........*/
+var tl = gsap.timeline();
+
+tl.from(".bounce", {duration: 2.5, ease: "bounce.out", y: -500 });
+tl.from(".footer__title", {duration: 1, x: 200, scale: 0.2});
+
+
+ft.reveal(".slide", {delay: 200}); 
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 800});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 900}); 
+sr.reveal('.footer__social',{interval: 900});
+
